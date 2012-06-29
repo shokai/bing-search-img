3f12f91fb56835ff8383d100bf4856aa70a96559
Sho Hashimoto <hashimoto@shokai.org>
2012-06-30 05:27:51 +0900

    index page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 
 exports.index = function(req, res){
   var app_root = (req.connection.encrypted ? 'https' : 'http') + '://' + req.headers['host'];
-  res.render('index', { title: 'Bing Search Image' })
+  res.render('index', { title: 'Bing Search Image', app_root: app_root })
 };
