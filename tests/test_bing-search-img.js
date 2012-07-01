7b4c727b6787fd5891034642fa96fac80156ac83
Sho Hashimoto <hashimoto@shokai.org>
2012-07-01 12:47:01 +0900

    exit test if BING_KEY not exists

--- tests/test_bing-search-img.js
+++ tests/test_bing-search-img.js
@@ -1,5 +1,9 @@
 var bing = require(__dirname+'/../libs/bing-search-img.js');
 
+if(!process.env.BING_KEY){
+  console.error('BING_KEY not exists.');
+  process.exit(1);
+}
 bing.api_key(process.env.BING_KEY);
 
 bing.search('book', function(results){
