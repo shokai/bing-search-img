6f0cb83e1b1e5dcd7b0a481b2db0a45a7ef216ad
Sho Hashimoto <hashimoto@shokai.org>
2012-07-01 21:47:44 +0900

    bugfix indexpage javascript

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,7 @@
 $(function(){
-  $('#sample input[name=search]').click(search);
+  $('#sample input[name=search]').click(function(e){
+    search();
+  });
   $('#sample input[name=word]').keydown(function(e){
     if(e.keyCode === 13) search();
   });
